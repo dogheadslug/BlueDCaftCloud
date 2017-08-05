d1f3575181d091a771d638436a5446cb85a1ceaf
Zhihao Hong <zhon4@ucsc.edu>
2017-08-05 13:57:30 -0700

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,8 +9,7 @@
  * This piece of code is based on Chad's template
  * *******************************************/
 
- //things need to figure out:
- //multithreading
+//next step: start the downloaded file after update complete
 
 using System;
 using System.IO;
@@ -32,19 +31,22 @@ using System.Threading;
 using System.Net;
 using System.ComponentModel;
 
-namespace BluedcraftCloudUpdate{
+namespace BluedcraftCloudUpdate
+{
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window{
+    public partial class MainWindow : Window
+    {
 
         //private static System.Timers.Timer aTimer;
         //private static int SomeInt = 20;
 
-        public MainWindow() {
+        public MainWindow()
+        {
             InitializeComponent();
 
-            
+
             //download file:
             //https://msdn.microsoft.com/en-us/library/ez801hhe(v=vs.110).aspx
             //the FOLDER path needed for DownloadFile
@@ -65,48 +67,47 @@ namespace BluedcraftCloudUpdate{
              * 
             System.Net.WebClient myWebClient = new System.Net.WebClient();
             myWebClient.OpenRead(myFileSrc);
-
             //size of download file
             Int64 DL_FileSize = Convert.ToInt64(myWebClient.ResponseHeaders["Content-Length"]);
-
             //size of local file
             Int64 LC_FileSize = Convert.ToInt64(new System.IO.FileInfo(FileName).Length);
-
             //while the file size do not match
             //use the while loop so it checks if the file has been successfully downloaded
             while (DL_FileSize != LC_FileSize) {
-
                 startDownload(myFileSrc, FileName);
                 //check if the file has been downloaded completely
                 LC_FileSize = Convert.ToInt64(new System.IO.FileInfo(FileName).Length);
             }
-
             ******************************/
 
             //after download is finished, launch the familyTong
-            //System.Diagnostics.Process.Start(FileName);
-            
-            //close window
-            this.Close();
+            System.Diagnostics.Process.Start(FileName);
+
+            //close window(not here!)
+            //this.Close();
         }
 
 
         //basic interface functions. title bar at the top
         //drag the window around
-        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e){
-            try{
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
                 this.DragMove();
             }
             catch { }
         }
 
         //close the window
-        private void Window_close(object sender, RoutedEventArgs e){
+        private void Window_close(object sender, RoutedEventArgs e)
+        {
             this.Close();
         }
 
         //minimize the window
-        private void Window_minimize(object sender, RoutedEventArgs e){
+        private void Window_minimize(object sender, RoutedEventArgs e)
+        {
             this.WindowState = WindowState.Minimized;
             //i have no idea how i figured it out
         }
@@ -114,7 +115,8 @@ namespace BluedcraftCloudUpdate{
         //uwp progress bar
         //https://social.msdn.microsoft.com/Forums/windows/en-US/d07047a7-bd9c-4f9e-b4a0-41f63164b769/c-httpclient-download-file-progress?forum=winforms
         //System.Net.WebClient m_WebClient;
-        private void startDownload(string URL, string fileName){
+        private void startDownload(string URL, string fileName)
+        {
             WebClient m_WebClient = new WebClient();
             m_WebClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(DownloadProgressChanged);
             m_WebClient.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadFileCompleted);
@@ -123,14 +125,19 @@ namespace BluedcraftCloudUpdate{
 
         }
 
-        private void DownloadProgressChanged(object sender, System.Net.DownloadProgressChangedEventArgs e) {
+        private void DownloadProgressChanged(object sender, System.Net.DownloadProgressChangedEventArgs e)
+        {
             //Update the progress bar value with the percentage 0-100
-            pBar1.Value =(int)e.ProgressPercentage;
+            pBar1.Value = (int)e.ProgressPercentage;
             //ProgressPercentage is avaliable so do not need to calculate percentage
         }
 
-        private void DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e) {
+        private void DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
             // System.Diagnostics.Process.Start(FileName);
+
+            //close the window after download is complete
+            //this.Close();
         }
 
         //https://stackoverflow.com/questions/12126889/how-to-use-winforms-progress-bar
